c63c6667cc4bc92e87be1c7778babf6aadbc5471
aalok125 <anupniroula125@gmail.com>
2020-04-29 21:43:56 +0545

    Category list display updated

--- votingsystem/vote/admin.py
+++ votingsystem/vote/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Category, Tag, Question, Answer, GuestUser, GuestUserLog, UserSearchLog, UserVote
 
 # Register your models here.
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image', 'parent', 'status']
+
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag)
 admin.site.register(Question)
 admin.site.register(Answer)
