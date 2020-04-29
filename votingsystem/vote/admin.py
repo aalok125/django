cd24eb7308154ec65caf4f844177aae6ad63194e
aalok125 <anupniroula125@gmail.com>
2020-04-29 22:25:35 +0545

    Category list display with image

--- votingsystem/vote/admin.py
+++ votingsystem/vote/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from .models import Category, Tag, Question, Answer, GuestUser, GuestUserLog, UserSearchLog, UserVote
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'image', 'parent', 'status']
+    list_display = ['title', 'category_image', 'parent', 'status']
+    readonly_fields = ['category_image']
 
+    # image show in admin list
+    def category_image(self, obj):
+        if obj.id:
+            return mark_safe('<img src="%s" height="150">' % obj.image.url)
+        return ''
+
+    category_image.allow_tags = True
 
 
 admin.site.register(Category, CategoryAdmin)
