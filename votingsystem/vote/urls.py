d4732a51d7003e236ee72c8ed4b1219b50d602c7
Your Name <bijayfriend45@gmail.com>
2020-05-01 02:07:53 +0545

    votingpage ajax

--- votingsystem/vote/urls.py
+++ votingsystem/vote/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HomeView.as_view()),
-    path('category/',views.CategoryView.as_view()),
-    path('voting/',views.VotingPageView.as_view()),
-    path('result/',views.VotingResultView.as_view()),
+    path('',views.HomeView.as_view(), name="home"),
+    path('category/<str:category_slug>',views.CategoryView.as_view(), name="category"),
+    path('poll/<str:question_slug>',views.PollPageView.as_view(), name="poll"),
+    path('result/<str:question_slug>',views.PollResultView.as_view(), name="result"),
+    path('poll/<str:question_slug>/filter_options',views.filter_options, name="filter_options"),
+    path('poll/<str:question_slug>/<str:answer_pk>',views.VoteSelectView.as_view(), name="vote_selection"),
+    
 ]
+
+handler404 = 'vote.views.error_404_view'
--- votingsystem/vote/views.py
+++ votingsystem/vote/views.py
@@ -1,29 +1,80 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, Http404
 from django.views import View
+from .models import *
+from random import randint
+from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 # def index(request):
 #     return HttpResponse("hello")
 
 class HomeView(View):
+    template_name = "front/index.html"
     def get(self, request):
-        context = {}
-        return render(request,"front/index.html")
+        categories = Category.objects.filter(parent=None)
+        for category in categories:
+            category.active = True
+            break
+        count = Question.objects.count()
+        trending_question = Question.objects.all()[randint(0, count - 1)]
+        trending_question.answers = trending_question.answer_set.all()[:5]
+        dictionary = {
+            'categories':categories,
+            'trending_question':trending_question,
+        }
+        return render(request,self.template_name, context=dictionary)
 
 
 class CategoryView(View):
+    template_name="front/category.html"
+    def get(self, request, category_slug):
+        try:
+            category = Category.objects.get(slug=category_slug)
+            dictionary = {
+                'category':category
+            }
+        except Category.DoesNotExist:
+            raise Http404
+        return render(request,self.template_name,context=dictionary)
+
+class PollPageView(View):
+    template_name = "front/votingPage.html"
+    def get(self, request, question_slug):
+        try:
+            question = Question.objects.get(slug=question_slug)
+            dictionary = {
+                'question':question
+            }
+        except Question.DoesNotExist:
+            raise Http404
+        return render(request,self.template_name, context=dictionary)
+
+class VoteSelectView(View):
+    def get(self, request, question_slug, answer_pk):
+        return HttpResponse(question_slug+""+answer_pk)
+
+class PollResultView(View):
+    template_name = "front/votingResult.html"
     def get(self, request):
         context = {}
-        return render(request,"front/category.html")
+        return render(request,self.template_name)
 
+def filter_options(request, question_slug):
+    try:
+        question = Question.objects.get(slug=question_slug)
+        search = request.POST['search_text']
+        if(search == ""):
+            answers = question.answer_set.all()[:8]
+            data = serializers.serialize("json", answers)
+        else:
+            answers = question.answer_set.filter(title__icontains=search)[:8] 
+            data = serializers.serialize("json", answers)
+    except Question.DoesNotExist:
+            raise Http404
+    
+    return HttpResponse(data, content_type='application/json')
 
-class VotingPageView(View):
-    def get(self, request):
-        context = {}
-        return render(request,"front/votingPage.html")
-
-
-class VotingResultView(View):
-    def get(self, request):
-        context = {}
-        return render(request,"front/votingResult.html")
+#Custom 404 error page
+def error_404_view(request, exception):
+    return HttpResponse("Page Not Found")
